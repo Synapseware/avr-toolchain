02a12685c7466eb7485edd33065bf6a8ddb4b5af
Ramun McCallum <matthew@synapseware.com>
2018-08-25 20:01:56 -0700

    Simplify toolchain installer

--- install-avr-build-chain-linux.sh
+++ install-avr-build-chain-linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [[ $EUID -ne 0 ]]; then
 	echo "Must be run as root"
 	exit -1
@@ -12,7 +14,22 @@ fi
 
 SRC=$1
 
-echo "Installing AVR GNU Toolchain from $SRC to /opt"
+VER=`echo $SRC | awk -F"-" '{ print $4 }'`
+NAME="avr8-gnu-toolchain-$VER"
+DST="/opt/$NAME"
 
-tar -xvf "$SRC" -C /opt/
+if [ -d "$DST" ]; then
+	echo "Destination folder exists.  Exiting."
+	exit -1
+fi
 
+echo "Installing AVR GNU Toolchain version $VER to $DST"
+
+echo "Extract installer to temp folder"
+tar -xf "$SRC" -C /tmp/
+
+echo "Creating install folder"
+mkdir -p "$DST"
+
+mv /tmp/avr8-gnu-toolchain-linux_x86_64/* "$DST/"
+rm -rf /tmp/avr8-gnu-toolchain-linux_x86_64
